167dcb1e510411e6a918af39bb61e8d98ac15505
scramados <jasper.kamerling@hotmail.com>
2020-05-04 21:50:36 +0200

    fix header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,11 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  navbarOpen = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { STUDENT_DATA } from '../../_services/student-data-service';
 
 @Component({
@@ -6,12 +6,22 @@ import { STUDENT_DATA } from '../../_services/student-data-service';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
   studentData = STUDENT_DATA;
+  currentSection = 'section1';
 
   constructor() { }
 
-  ngOnInit(): void {
+
+  onSectionChange(sectionId: string) {
+    this.currentSection = sectionId;
+  }
+
+  scrollTo(section) {
+    document.querySelector('#' + section)
+      .scrollIntoView();
   }
 
 }
