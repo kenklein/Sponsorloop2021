8f534793f07daa2b283d166fd58a544754b201b0
Kenenth Lee Klein <k.klein@outlook.com>
2020-05-17 23:47:29 +0200

    Update layout and information for the site

--- src/_services/student-data-service.ts
+++ src/_services/student-data-service.ts
@@ -2,46 +2,10 @@ import {Student} from '../_models/student';
 
 export const STUDENT_DATA: Student[] = [
   {
-    name: 'Ken Klein',
+    name: 'Deelnemer #1',
     targetAmount: 100,
-    actualAmount: 1,
-    description: 'Ken heeft zijn geld ingezameld bij zijn ouders en familie en zal streven voor de 10KM'
-  },
-  {
-    name: 'Force 1 ',
-    targetAmount: 2,
-    actualAmount: 10,
-    description: 'Lorem Ipsum'
-  },
-  {
-    name: 'Force 2',
-    targetAmount: 3,
-    actualAmount: 1,
-    description: 'Lorem Ipsum'
-  },
-  {
-    name: 'Force 3',
-    targetAmount: 4,
     actualAmount: 0,
-    description: 'Lorem Ipsum'
-  },
-  {
-    name: 'Force 4',
-    targetAmount: 5,
-    actualAmount: 1,
-    description: 'Lorem Ipsum'
-  },
-  {
-    name: 'Force 5',
-    targetAmount: 90,
-    actualAmount: 10,
-    description: 'Lorem Ipsum'
-  },
-  {
-    name: 'EX - Force 1',
-    targetAmount: 7,
-    actualAmount: 10,
-    description: 'Lorem Ipsum'
+    description: 'Waarom doe jij mee ?'
   },
 
 ];
