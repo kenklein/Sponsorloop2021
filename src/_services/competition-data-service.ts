025f27843a6fd4222f32b13194ce61cd61a21732
Kenenth Lee Klein <k.klein@outlook.com>
2020-06-03 14:52:08 +0200

    Data update 3-6-2020 change actualamount UTV

--- src/_services/competition-data-service.ts
+++ src/_services/competition-data-service.ts
@@ -5,7 +5,7 @@ export const COMPETITION_DATA: Competition[] = [
     name: 'Competitie',
     description: 'Overzicht van de gelopen kilometers tussen de verenigingen (U.T.V. vs ICB) en overige deelnemers',
     targetAmountutv: 50,
-    actualAmountutv: 39.66,
+    actualAmountutv: 44.41,
     targetAmounticb: 50,
     actualAmounticb: 35.41,
     targetAmountother: 50,
