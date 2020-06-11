84758db51c41cf004be31d2922f154656c8d6a4d
Kenenth Lee Klein <k.klein@outlook.com>
2020-06-11 10:51:25 +0200

    Data update 11-6 10.51

--- src/_services/competition-data-service.ts
+++ src/_services/competition-data-service.ts
@@ -5,7 +5,7 @@ export const COMPETITION_DATA: Competition[] = [
     name: 'Competitie',
     description: 'Overzicht van de gelopen kilometers tussen de verenigingen (U.T.V. vs ICB) en overige deelnemers',
     targetAmountutv: 70,
-    actualAmountutv: 48.71,
+    actualAmountutv: 57.44,
     targetAmounticb: 70,
     actualAmounticb: 60.41,
     targetAmountother: 70,
