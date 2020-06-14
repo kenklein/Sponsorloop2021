d68041656c8e6b63d9c281a3556c14a0bdcec86d
Kenenth Lee Klein <k.klein@outlook.com>
2020-06-14 23:47:21 +0200

    Data update 13-6-2020 20.12

--- src/_services/facebook-service.ts
+++ src/_services/facebook-service.ts
@@ -1,5 +1,6 @@
 import {Facebook} from '../_models/facebook';
 
 export const FACEBOOK_DATA: Facebook = {
-  content : 'De sponsorloop is ten einde gekomen. Wij willen alle deelnemers en sposnoren bedanken voor hun inzet voor dit evenement !',
+  content : 'De sponsorloop is ten einde gekomen.'+
+    ' Wij willen alle deelnemers en sposnoren bedanken voor hun inzet voor dit evenement !',
 }
