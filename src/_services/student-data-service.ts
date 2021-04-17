802622cb58fec8144a04afe6ab6171ef790c6633
Ken Klein <k.klein@outlook.com>
2021-04-17 23:57:35 +0200

    Run update 17-4-2021

--- src/_services/student-data-service.ts
+++ src/_services/student-data-service.ts
@@ -2,6 +2,14 @@ import {Student} from '../_models/student';
 
 export const STUDENT_DATA: Student[] = [
   {
+    name: 'Jildou Valkema',
+    team: 'U.T.V.',
+    amount: 6,
+    amountRaised: 6,
+    description: '',
+    imageSource: 'assets/runs/run_jildou_valkema.png',
+  },
+  {
     name: 'Lisa de Winter',
     team: 'U.T.V.',
     amount: 8,
@@ -10,5 +18,6 @@ export const STUDENT_DATA: Student[] = [
       'Standaard elke zaterdag 20km rennen met elkaar. Dat noem ik pas doorzetten ❤️ ' +
       'Ooit in de verre toekomst loop ik een keer met ze mee ! #strongertogether',
     imageSource: 'assets/runs/run_lisa_de_winter.png',
-  }
+  },
+
 ];
