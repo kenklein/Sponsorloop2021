4b0fa20d2ed3d1839ae442b8ab86c3a2ed497af1
Ken Klein <k.klein@outlook.com>
2021-04-26 01:13:05 +0200

    Change service, set photo's

--- src/_models/carousel.ts
+++ src/_models/carousel.ts
@@ -1,6 +1,4 @@
 export interface Carousel {
-  title: string;
-  description: string;
   buttontitle: string;
   buttonurl: string;
   imageScr: string;
--- src/_services/carousel-data-service.ts
+++ src/_services/carousel-data-service.ts
@@ -2,17 +2,28 @@ import {Carousel} from "../_models/carousel";
 
 export const CAROUSEL_DATA: Carousel[] = [
   {
-    title: 'test_service',
-    description: 'test service description',
-    buttontitle: 'link',
-    buttonurl: 'https://www.google.nl/',
+    buttontitle: null,
+    buttonurl: null,
+    imageScr: 'assets/action_run/action_maartje_brok.png',
+  },
+  {
+    buttontitle: null,
+    buttonurl: null,
+    imageScr: 'assets/action_run/action_sofie_van_leeuwen.png',
+  },
+  {
+    buttontitle: null,
+    buttonurl: null,
+    imageScr: 'assets/action_run/action_machteld_menkveld.png',
+  },
+  {
+    buttontitle: null,
+    buttonurl: null,
     imageScr: 'assets/action_run/action_jesse_van_kammen.png',
   },
   {
-    title: 'test_service_2',
-    description: 'test service description',
     buttontitle: null,
     buttonurl: null,
-    imageScr: 'assets/runs/run_didier_wouters.png',
+    imageScr: 'assets/action_run/action_tessa_klomp.png',
   },
 ];
